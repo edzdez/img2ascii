c7ad3b71ace376c826cf71a134d4e2f49e5f3ec2
Ethan Zhang <ezsland05@gmail.com>
2021-12-12 11:29:22 -0800

    Added the 'anyhow' library to give more readable errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use anyhow::{Context, Result};
+use image::io::Reader;
+use image::{DynamicImage, ImageResult};
 use std::error::Error;
 use std::path::PathBuf;
-use image::{DynamicImage, ImageResult};
-use image::io::Reader;
 use structopt::StructOpt;
 
 use crate::options::Options;
@@ -9,15 +10,14 @@ use crate::options::Options;
 mod options;
 
 fn read_image(filepath: &PathBuf) -> ImageResult<DynamicImage> {
-    Ok(Reader::open(filepath)?
-        .with_guessed_format()?
-        .decode()?)
+    Ok(Reader::open(filepath)?.with_guessed_format()?.decode()?)
 }
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<()> {
     let opts = Options::from_args();
 
-    let image = read_image(&opts.input)?;
+    let image = read_image(&opts.input)
+        .with_context(|| format!("could not open image {:?}", &opts.input))?;
 
     Ok(())
 }
